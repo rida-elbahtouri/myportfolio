de83fc789dfa24fdbf74cc6363f99979b2588d53
rida-elbahtouri <elbahtouri0123@gmail.com>
2020-10-18 00:17:05 +0100

    create bottomnav component

---
+++ src/component/bottomNav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+const BottomNav=()=> {
+    return (
+        <div className="navbar">
+            <ul className="bottomnav"  >
+                <li className="hmm clr"><span>Home</span></li>
+                <li className="hmm"><span>Skills</span></li>
+                <li className="hmm"><span>Projects</span></li>
+            </ul>
+        </div>
+    )
+}
+export default BottomNav
